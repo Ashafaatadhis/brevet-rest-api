7bab25317040339dd9c34ebb7ce7b89dc0b59b32
ashafaatadhis <skullreaper387@gmail.com>
2024-02-21 21:28:34 +0700

    add vercel.json

--- server.ts
+++ server.ts
@@ -22,7 +22,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cookieParser());
-app.use(express.static(__dirname + "/public"));
+
 app.use(express.json({}));
 app.use(express.urlencoded({ extended: true }));
 
